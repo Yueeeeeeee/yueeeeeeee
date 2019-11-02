984cab72b2cf59c588672bb1cf5ada0489c081a6
Zhenrui Yue <yuezrhb@gmail.com>
2019-11-02 16:34:19 -0700

    update single blog view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import BlogPost
+import datetime
 
 def post_list(request):
     posts = BlogPost.objects.all()
@@ -7,4 +8,18 @@ def post_list(request):
         request,
         'blog/post/list.html',
         {'posts': posts}
+    )
+
+def post_detail(request, year, month, day, slug):
+    post = BlogPost.objects.filter(
+        publish=datetime.date(year, month, day),
+        #publish__year=year,
+        #publish__month=month,
+        #publish__day=day,
+        slug=slug
+    ).first()
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post}
     )
