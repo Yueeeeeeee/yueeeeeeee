310114b98313948ece0358eb5b0cbac59f5e6591
Zhenrui Yue <yuezrhb@gmail.com>
2019-11-23 00:32:39 -0800

    update deployment settings

--- yueeeeeeee/wsgi.py
+++ yueeeeeeee/wsgi.py
@@ -11,6 +11,15 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yueeeeeeee.settings')
+# start adding settings
+import sys                                                                                                                                                    
+from os.path import dirname, abspath                                                                                                                    
+from django.core.wsgi import get_wsgi_application                                                                                                             
+PROJECT_DIR = dirname(dirname(abspath(__file__)))                                                                                                             
+sys.path.insert(0, PROJECT_DIR)                                                                                                                               
+                                                                                                                                                              
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yueeeeeeee.settings')                                                                                   
+os.environ["DJANGO_SETTINGS_MODULE"] = "yueeeeeeee.settings"                                                                                               
+# end
 
 application = get_wsgi_application()
