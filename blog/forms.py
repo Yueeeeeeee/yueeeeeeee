22119a1ae9c73d5f875df585ade5f331a8b3e2b9
Zhenrui Yue <yuezrhb@gmail.com>
2019-11-22 17:43:38 -0800

    update comment window

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import BlogComment
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = BlogComment
+        fields = ('username', 'email', 'body')
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from blog.models import BlogPost
+from blog.forms import CommentForm
 import datetime
 
 def post_list(request):
     post_list = BlogPost.objects.all()
-    paginator = Paginator(post_list, 1) # how many blogs per page
+    paginator = Paginator(post_list, 10) # how many blogs per page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -21,15 +22,27 @@ def post_list(request):
 
 def post_detail(request, year, month, day, slug):
     post = BlogPost.objects.filter(
-        #publish__date__gt=datetime.date(year, month, day),
-        #publish__year=year,
-        #publish__month=month,
-        #publish__day=day,
         publish=datetime.date(year, month, day),
         slug=slug
     ).first()
+    comments = post.blog_comments.filter(active=True) # retrieve active comments
+    new_comment = None
+    comment_form = CommentForm() # Create comment form for new comment
+    if request.method == 'POST': # For request type 'POST', save comment if valid
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+        #else:
+            #comment_form = CommentForm()
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post}
+        {
+            'post': post,
+            'comments': comments,
+            'new_comment': new_comment,
+            'comment_form': comment_form
+        }
     )
