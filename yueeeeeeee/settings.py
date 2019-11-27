0feb3047e0bd7d0f75335b3f9daab0b18c93d069
Zhenrui Yue <yuezrhb@gmail.com>
2019-11-27 00:36:01 -0800

    fix chinese language error

--- yueeeeeeee/settings.py
+++ yueeeeeeee/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sys
+
+default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
